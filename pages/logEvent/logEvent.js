9246038fbf4a559053adde70782b44c5e95fd4b8
xiapeimin <xpm2536733090>
2020-05-04 19:08:44 +0800

    ui_i页面_我的记录&添加&我的

--- pages/logEvent/logEvent.js
+++ pages/logEvent/logEvent.js
@@ -1,66 +1,97 @@
-// pages/logEvent/logEvent.js
+const app = getApp();
+var timestamp = Date.parse(new Date());
+var date = new Date(timestamp);
+var Y = date.getFullYear();
+var M = (date.getMonth() + 1 < 10 ? '0' + (date.getMonth() + 1) : date.getMonth() + 1);
+var D = date.getDate() < 10 ? '0' + date.getDate() : date.getDate();
+
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    StatusBar: app.globalData.StatusBar,
+    CustomBar: app.globalData.CustomBar,
+    index: null,
+    picker: ['时事', '新闻', '生活','旅游','情感'],
+    bgColor: ['bg-red', 'bg-orange', 'bg-green', 'bg-pink','bg-purple'],
+    changebg:'bg-blue',
+    multiIndex: [0, 0, 0],
+    date: Y+'-'+M+'-'+D,
+    imgList: [],
+    modalName: null,
+    tagtxt:'默认'
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  PickerChange(e) {
+    console.log(e);
+    this.setData({
+      index: e.detail.value
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  MultiChange(e) {
+    this.setData({
+      multiIndex: e.detail.value
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+  DateChange(e) {
+    this.setData({
+      date: e.detail.value
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
+  ChooseImage() {
+    wx.chooseImage({
+      count: 4, //默认9
+      sizeType: ['original', 'compressed'], //可以指定是原图还是压缩图，默认二者都有
+      sourceType: ['album'], //从相册选择
+      success: (res) => {
+        if (this.data.imgList.length != 0) {
+          this.setData({
+            imgList: this.data.imgList.concat(res.tempFilePaths)
+          })
+        } else {
+          this.setData({
+            imgList: res.tempFilePaths
+          })
+        }
+      }
+    });
   },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
+  ViewImage(e) {
+    wx.previewImage({
+      urls: this.data.imgList,
+      current: e.currentTarget.dataset.url
+    });
   },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
+  DelImg(e) {
+    wx.showModal({
+      title: '',
+      content: '确定要删除这张图片吗？',
+      cancelText: '取消',
+      confirmText: '确定',
+      success: res => {
+        if (res.confirm) {
+          this.data.imgList.splice(e.currentTarget.dataset.index, 1);
+          this.setData({
+            imgList: this.data.imgList
+          })
+        }
+      }
+    })
   },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
+  showModal(e) {
+    this.setData({
+      modalName: e.currentTarget.dataset.target
+    })
   },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  hideModal(e) {
+    this.setData({
+      modalName: null
+    })
+  },
+  checkTag(e){
+    let id = e.currentTarget.id.slice(3);
+    let value = this.data.picker[id];
+    let bgColor = this.data.bgColor[id];
+    this.setData({
+      tagtxt: value,
+      changebg:bgColor
+    });
   }
 })
