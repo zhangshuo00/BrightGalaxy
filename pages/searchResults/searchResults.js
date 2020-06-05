199d89890fb13a87cd2b73a230e520d9701f7cfd
zhangshuo <443494891@qq.com>
2020-06-05 17:54:20 +0800

    统一页面布局样式

--- pages/searchResults/searchResults.js
+++ pages/searchResults/searchResults.js
@@ -29,6 +29,12 @@ Page({
         }
       })
     },
+    jumpToFeedback: function(){
+      // 未搜索到相应的结果，点击反馈，携带搜索词，跳转到反馈页面
+      wx.navigateTo({
+        url: '/pages/feedback/feedback',
+      })
+    },
 
     /**
      * 生命周期函数--监听页面加载
